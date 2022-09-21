57aef2913f19f255433733bbfd2fb58df8d3d2ee
Ajsteph89 <ajsteph89@gmail.com>
2022-09-21 13:29:27 -0400

    add basic models

--- ecard/models.py
+++ ecard/models.py
@@ -6,3 +6,40 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Card(models.Model):
+    owner = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'cards')
+    title = models.CharField(max_length=300)
+    outer_message = models.TextField()
+    inner_message = models.TextField()
+    style = models.ForeignKey('Style',on_delete=models.CASCADE, related_name = 'cards')
+
+    def __str__(self):
+        return f'{self.title} by: {self.owner}'
+
+class Style(models.Model):
+    style = models.CharField (max_length=300)
+
+    def __str__(self):
+        return f'{self.style}'
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'comments')
+    card = models.ForeignKey(Card,on_delete=models.CASCADE, related_name = 'comments')
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.text} by:{self.owner} on {self.card}'
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'following')
+    followee = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'followers')
+
+    def __str__(self):
+        return f'{self.follower} following {self.followee}'
+
+
+
+
