53e893b02e08aa2e1693a123b631b14fc89785db
StephenTurk90 <stephenjturk@gmail.com>
2022-09-28 09:48:54 -0400

    added api root from a merge-branch

--- ecard/urls.py
+++ ecard/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from ecard import views
 
 urlpatterns = [
-<<<<<<< HEAD
+    path('', views.api_root),
     path('ecard/', views.CardList.as_view()),
     path('ecard/<int:pk>/', views.CardDetail.as_view()),
     path('ecard/user/', views.UserCardList.as_view()),
@@ -13,16 +13,6 @@ urlpatterns = [
     path('styles/', views.StyleCreate.as_view()),
     path('following/', views.FollowList.as_view()),
     path('following/delete/<int:pk>/', views.RemoveFollow.as_view()),
-=======
-    path('', views.api_root),
-    path('ecard/', views.CardList.as_view(), name='ecard_list'),
-    path('ecard/<int:pk>/', views.CardDetail.as_view(), name='ecard_detail'),
-    path('ecard/user/', views.UserCardList.as_view(), name='user_ecards'),
-    path('comments/', views.CommentList.as_view(), name='comment_list'),
-    path('comments/<int:pk>/', views.CommentDetail.as_view(), name='comment_detail'),
-    path('comments/user/', views.CommentDetail.as_view(), name='user_comments'),
-    path('styles/', views.StyleCreate.as_view(), name='styles'),
->>>>>>> main
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
