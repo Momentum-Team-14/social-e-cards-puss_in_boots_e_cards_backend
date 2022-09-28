3b75f4d3e05dc52feb76f94058468c16ba845d7b
StephenTurk90 <stephenjturk@gmail.com>
2022-09-28 11:32:11 -0400

    update urls

--- ecard/urls.py
+++ ecard/urls.py
@@ -4,13 +4,13 @@ from ecard import views
 
 urlpatterns = [
     path('', views.api_root),
-    path('ecard/', views.CardList.as_view()),
-    path('ecard/<int:pk>/', views.CardDetail.as_view()),
-    path('ecard/user/', views.UserCardList.as_view()),
-    path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>/', views.CommentDetail.as_view()),
-    path('comments/user/', views.CommentDetail.as_view()),
-    path('styles/', views.StyleCreate.as_view()),
+    path('ecard/', views.CardList.as_view(), name='ecard_list'),
+    path('ecard/<int:pk>/', views.CardDetail.as_view(), name='ecard_detail'),
+    path('ecard/user/', views.UserCardList.as_view(), name='user_ecards'),
+    path('comments/', views.CommentList.as_view(), name='comment_list'),
+    path('comments/<int:pk>/', views.CommentDetail.as_view(), name='comment_detail'),
+    path('comments/user/', views.CommentDetail.as_view(), name='user_comments'),
+    path('styles/', views.StyleCreate.as_view(), name='styles'),
     path('following/', views.FollowList.as_view()),
     path('following/delete/<int:pk>/', views.RemoveFollow.as_view()),
     path('users/', views.UserList.as_view, )
